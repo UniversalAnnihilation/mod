e96a35bf3caacec4f3cb25137ed2aa6d6ef21b03
Jason <jdrake.nz@gmail.com>
2010-12-17 03:28:20 +1300

    Fixed cost rounding causing unbuildable solars

--- gamedata/unitdefs_post.lua
+++ gamedata/unitdefs_post.lua
@@ -3,6 +3,7 @@ local floor = math.floor
 local log10 = math.log10
 
 function Round(num, digits)
+	if num == 0 then return 0 end
 	local scale = 10 ^ floor(log10(num) + 1 - digits)
 	return scale * floor(num / scale + 0.5)
 end
