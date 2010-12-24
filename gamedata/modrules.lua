2ecc638c83cea20f5e54e3234775f156225fe506
Jason <jdrake.nz@gmail.com>
2010-12-24 23:33:03 +1300

    Removal of all energy variables from all units

--- gamedata/modrules.lua
+++ gamedata/modrules.lua
@@ -1,30 +1,35 @@
 local modrules  = {
-
-  reclaim = {
-    multiReclaim  = 1;
-    reclaimMethod = 0;
-  },
-
-
-  sensors = {   
-    los = {
-      losMipLevel = 2, 
-      losMul      = 1,
-      airMipLevel = 4,
-      airMul      = 1,
-    },
-  },
-
-  fireAtDead = {
-    fireAtKilled   = false;
-    fireAtCrashing = false;
-  },
-
-  nanospray = {
-    allow_team_colors = true;
-  },
-  
-  featureLOS = { featureVisibility = 2; }
+	
+	reclaim = {
+		multiReclaim  = 1;
+		reclaimMethod = 0;
+	},
+	
+	resurrect = {
+		energyCostFactor = 0;
+	},
+	
+	sensors = {   
+		los = {
+			losMipLevel = 2, 
+			losMul      = 1,
+			airMipLevel = 4,
+			airMul      = 1,
+		},
+	},
+	
+	fireAtDead = {
+		fireAtKilled   = false,
+		fireAtCrashing = false,
+	},
+	
+	nanospray = {
+		allow_team_colors = true,
+	},
+	
+	featureLOS = {
+		featureVisibility = 2,
+	},
 }
 
 --------------------------------------------------------------------------------
--- gamedata/unitdefs_post.lua
+++ gamedata/unitdefs_post.lua
@@ -2,7 +2,7 @@
 local floor = math.floor
 local log10 = math.log10
 
-function Round(num, digits)
+local function Round(num, digits)
 	if num == 0 then return 0 end
 	local scale = 10 ^ floor(log10(num) + 1 - digits)
 	return scale * floor(num / scale + 0.5)
@@ -10,6 +10,6 @@ end
 
 for uDefID, uDef in pairs(UnitDefs) do
 	uDef.buildcostmetal = Round(uDef.buildcostmetal, 2)
-	uDef.buildcostenergy = Round(uDef.buildcostenergy, 2)
-	uDef.buildtime = Round(uDef.buildtime, 2)
+	--uDef.buildcostenergy = 0
+	uDef.buildtime = 10 * uDef.buildcostmetal
 end
